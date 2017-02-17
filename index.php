8c80e208379079e5b9ffaecaf43e5f84cc455b37
jcchauvel <jcchauvel27@gmail.com>
2017-02-17 12:10:48 +0100

    no message

--- index.php
+++ index.php
@@ -4,10 +4,8 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="description" content="">
-        <meta name="author" content="">
         <link href="css/alice.css" rel="stylesheet">
-        <title>ALICE</title>
+        <title>Index ALICE</title>
     </head>
     <body>
         <div>
