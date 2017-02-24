ec100199d50ee45c98ac6f46bf50c63634b2611a
Vincent Schlosser <s.vincent@live.fr>
2017-02-24 12:23:57 +0100

    Ajout des header

--- vues/mod_Agent.php
+++ vues/mod_Agent.php
@@ -6,46 +6,13 @@ require_once('../class/agent.php');
 require_once('../include/alice_fonctions.php');
 require_once('../include/alice_dao.inc.php');
 ?>
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- Bootstrap Core CSS -->
-    <link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <!-- MetisMenu CSS -->
-    <link href="../bootstrap/css/metisMenu.min.css" rel="stylesheet">
-    <!-- Custom CSS -->
-    <link href="../bootstrap/css/sb-admin-2.css" rel="stylesheet">
-    <!-- Custom Fonts -->
-    <link href="../bootstrap/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-    <!-- Alice CSS -->
-    <link href="../css/alice.css" rel="stylesheet">
-    <!-- Chemin vers les librairies JavaScript -->
-    <script src="../include/alice.js"></script>
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-    <title>Modification des agents</title>
-</head>
-<header>
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-lg-2">
-                <img class="logo" src="../images/logo_sna_quadri.png"/>
-            </div>
-            <!-- Affichage du titre de la page -->
-            <div class="col-lg-offset-2 col-md-4">
-                <h2>Modification des agents</h2>
-            </div>
-            <?php include("../include/header_admin.php"); ?>
-        </div>
-    </div>
-</header>
+
+<?php include("../include/doctype.php"); ?>
+<!-- Affichage du titre de la page -->
+<div class="col-lg-offset-2 col-md-4">
+    <h2>Modification des agents</h2>
+</div>
+<?php include("../include/header_admin.php"); ?>
 
 <?php
 // Création d'un objet agent
@@ -96,7 +63,7 @@ if (isset($_POST['annuler'])) {// Cas du bouton vert "annuler"
     // die('<META HTTP-equiv="refresh" content=0;URL=admin_modif_plan.php>');
 }
 ?>
-
+<body>
 <!-- Affichage des agents -->
 <div class="container-flui col-lg-offset-1 col-lg-10">
     <table class="table table-bordered">
